e088d28f7ca72beecc6029d60063070e60b62cff
judy <kje010207@gmail.com>
2023-07-10 09:48:26 +0900

    judy, Update:: SizedBox 추가 width 기본값으로 줌

--- lib/widgets/accordion.dart
+++ lib/widgets/accordion.dart
@@ -5,6 +5,8 @@ import 'package:sfac_designkit_flutter/util/sfac_color.dart';
 class SfAccordion extends StatefulWidget {
   const SfAccordion({
     super.key,
+    this.width = double.infinity,
+    this.height,
     this.iconButton,
     required this.title,
     required this.content,
@@ -13,13 +15,16 @@ class SfAccordion extends StatefulWidget {
     this.contentPadding,
     this.contentMargin,
   });
+  final double? width; //아코디언 가로 넓이
+  final double? height; //아코디언 높이
   final Widget? iconButton; //  아이콘
-  final Text title; // 접었을 때 보이는 텍스트
+  final Widget title; // 접었을 때 보이는 텍스트
   final Widget content; // 펼쳤을 때 보이는 텍스트
   final Color? contentBackgroundColor; //펼쳤을 때 보이는 텍스트 상자 배경색
   final double contentRadius; //펼쳤을 때 보이는 텍스트 상자 곡선
   final EdgeInsetsGeometry? contentPadding; //펼쳤을 때 보이는 텍스트 상자 패딩
   final EdgeInsetsGeometry? contentMargin; // 펼쳤을 때 보이는 텍스트 상자 마진
+
   @override
   State<SfAccordion> createState() => _SfAccordionState();
 }
@@ -45,43 +50,47 @@ class _SfAccordionState extends State<SfAccordion> {
       child: widget.content,
     );
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            GestureDetector(
-                onTap: () {
-                  isVisible = !isVisible;
-                  setState(() {});
-                },
-                child: isVisible
-                    ? Transform.rotate(
-                        angle: 90 * 3.1415926535 / 180,
-                        child:
-                            widget.iconButton ?? const Icon(Icons.play_arrow))
-                    : widget.iconButton ?? const Icon(Icons.play_arrow)),
-            const SizedBox(width: 5),
-            Flexible(child: titleText),
-          ],
-        ),
-        isVisible
-            ? Container(
-                margin: widget.contentMargin ??
-                    const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                padding: widget.contentPadding ?? const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color:
-                        widget.contentBackgroundColor ?? SfacColor.grayScale5,
-                    borderRadius: BorderRadius.all(
-                        Radius.circular(widget.contentRadius))),
-                child: contentText,
-              )
-            : const SizedBox(),
-      ],
+    return SizedBox(
+      width: widget.width,
+      height: widget.height,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              GestureDetector(
+                  onTap: () {
+                    isVisible = !isVisible;
+                    setState(() {});
+                  },
+                  child: isVisible
+                      ? Transform.rotate(
+                          angle: 90 * 3.1415926535 / 180,
+                          child:
+                              widget.iconButton ?? const Icon(Icons.play_arrow))
+                      : widget.iconButton ?? const Icon(Icons.play_arrow)),
+              const SizedBox(width: 5),
+              Flexible(child: titleText),
+            ],
+          ),
+          isVisible
+              ? Container(
+                  margin: widget.contentMargin ??
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  padding: widget.contentPadding ?? const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      color:
+                          widget.contentBackgroundColor ?? SfacColor.grayScale5,
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(widget.contentRadius))),
+                  child: contentText,
+                )
+              : const SizedBox(),
+        ],
+      ),
     );
   }
 }
