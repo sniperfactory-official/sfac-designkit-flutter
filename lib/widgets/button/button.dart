ec235bca3bf17ef6fe207c63e459a1a98dca1a93
judy <kje010207@gmail.com>
2023-07-13 16:26:05 +0900

    judy, Feat:: 버튼에 as child 기능 추가

--- lib/widgets/button/button.dart
+++ lib/widgets/button/button.dart
@@ -11,28 +11,59 @@ class SFButton extends StatefulWidget {
     this.disabledTextColor,
     this.disabledbackgroundColor,
     this.width,
-    this.height = 50,
+    this.height,
     this.outlineWidth = 1.0,
     this.outlineRadius = 10,
     this.hoverTextStyle,
     this.hoverbackgroundColor,
     this.isLink = false,
+    this.isAsChild = false,
     required this.child,
   });
-  final Widget? child; // 자식 위젯
-  final Color? backgroundColor; // 배경 색
-  final Color? textColor; // 텍스트 색
-  final Color? outlineColor; // 테두리 색
-  final void Function()? onPressed; // 버튼 함수
-  final Color? disabledTextColor; // 비활성화 텍스트 색
-  final Color? disabledbackgroundColor; // 비활성화 배경 색
-  final double? width; // 버튼 가로 넓이
-  final double? height; // 버튼 높이
-  final double outlineWidth; // 테두리 두께
-  final double outlineRadius; // 테두리 곡선
-  final TextStyle? hoverTextStyle; // 호버 텍스트 스타일
-  final Color? hoverbackgroundColor; // 호버 배경색
-  final bool isLink; // 링크 버튼(밑줄) true false
+  // 자식 위젯
+  final Widget? child;
+
+  // 배경 색
+  final Color? backgroundColor;
+
+  // 텍스트 색
+  final Color? textColor;
+
+  // 테두리 색
+  final Color? outlineColor;
+
+  // 버튼 함수
+  final void Function()? onPressed;
+
+  // 비활성화 텍스트 색
+  final Color? disabledTextColor;
+
+  // 비활성화 배경 색
+  final Color? disabledbackgroundColor;
+
+  // 버튼 가로 넓이
+  final double? width;
+
+  // 버튼 높이
+  final double? height;
+
+  // 테두리 두께
+  final double outlineWidth;
+
+  // 테두리 곡선
+  final double outlineRadius;
+
+  // 호버 텍스트 스타일
+  final TextStyle? hoverTextStyle;
+
+  // 호버 배경색
+  final Color? hoverbackgroundColor;
+
+  // 링크 버튼(밑줄) true false
+  final bool isLink;
+
+  // child 크기에 맞춤 크기
+  final bool isAsChild;
 
   @override
   State<SFButton> createState() => _SFButtonState();
@@ -67,32 +98,40 @@ class _SFButtonState extends State<SFButton> {
         child: widget.child!,
       );
     }
-    return InkWell(
-      onTap: widget.onPressed,
-      onHover: (value) {
-        ishover = value;
-        setState(() {});
-      },
-      borderRadius: BorderRadius.all(
-        Radius.circular(widget.outlineRadius),
-      ),
-      hoverColor: widget.hoverbackgroundColor ?? Colors.transparent,
-      child: Ink(
-        width: widget.width,
-        height: widget.height,
-        decoration: BoxDecoration(
-          color: widget.isLink
-              ? Colors.transparent
-              : widget.onPressed == null
-                  ? widget.disabledbackgroundColor ?? SFColor.grayScale5
-                  : widget.backgroundColor ?? SFColor.primary100,
-          borderRadius: BorderRadius.circular(widget.outlineRadius),
-          border: Border.all(
-            color: widget.outlineColor ?? Colors.transparent,
-            width: widget.outlineWidth,
-          ),
+    return FittedBox(
+      child: InkWell(
+        onTap: widget.onPressed,
+        onHover: (value) {
+          ishover = value;
+          setState(() {});
+        },
+        borderRadius: BorderRadius.all(
+          Radius.circular(widget.outlineRadius),
         ),
-        child: Center(child: childText),
+        hoverColor: widget.hoverbackgroundColor ?? Colors.transparent,
+        child: Ink(
+          width: widget.isAsChild
+              ? null
+              : widget.width ?? MediaQuery.of(context).size.width,
+          height: widget.isAsChild ? null : widget.height ?? 50,
+          decoration: BoxDecoration(
+            color: widget.isLink
+                ? Colors.transparent
+                : widget.onPressed == null
+                    ? widget.disabledbackgroundColor ?? SFColor.grayScale5
+                    : widget.backgroundColor ?? SFColor.primary100,
+            borderRadius: BorderRadius.circular(widget.outlineRadius),
+            border: Border.all(
+              color: widget.outlineColor ?? Colors.transparent,
+              width: widget.outlineWidth,
+            ),
+          ),
+          child: Center(
+              child: Padding(
+            padding: EdgeInsets.all(widget.isAsChild ? 10 : 0),
+            child: childText,
+          )),
+        ),
       ),
     );
   }
