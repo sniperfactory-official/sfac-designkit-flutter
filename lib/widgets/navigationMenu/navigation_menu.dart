711d0a3c45570ce8a7b86d28d683b2b6d52601eb
judy <kje010207@gmail.com>
2023-07-25 15:23:35 +0900

    judy, Feat:: NavigationMenu 위젯 추가 #62

---
+++ lib/widgets/navigationMenu/navigation_menu.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:sfac_design_flutter/sfac_design_flutter.dart';
+
+class SFNavigationMenu extends StatefulWidget {
+  const SFNavigationMenu({
+    super.key,
+    required this.menu,
+    this.width,
+    required this.height,
+    this.menuWidth,
+    this.menuHeigh,
+    this.menuSpacing = 5,
+    this.backgroundColor,
+    this.focusedBackgroundColor,
+    this.textColor,
+    this.focusedTextColor,
+    this.outlineColor,
+    this.radius = 10,
+    this.outlineWidth,
+    this.direction = Axis.horizontal,
+    this.onTap,
+    this.padding,
+    this.physics,
+  });
+  // 리스트 메뉴
+  final List<String> menu;
+
+  // 가로 넓이
+  final double? width;
+
+  // 높이
+  final double height;
+
+  // 메뉴 가로 넓이
+  final double? menuWidth;
+
+  // 메뉴 높이
+  final double? menuHeigh;
+
+  // 메뉴 사이 spacing
+  final double? menuSpacing;
+
+  // 메뉴 배경 색
+  final Color? backgroundColor;
+
+  // 포커스된 메뉴 배경 색
+  final Color? focusedBackgroundColor;
+
+  // 메뉴 텍스트 색
+  final Color? textColor;
+
+  // 포커스된 메뉴 텍스트 색
+  final Color? focusedTextColor;
+
+  // 테두리 색
+  final Color? outlineColor;
+
+  // 테두리 곡선
+  final double radius;
+
+  // 테두리 굵기
+  final double? outlineWidth;
+
+  // 메뉴 정렬 방향
+  final Axis direction;
+
+  // 메뉴 클릭 함수
+  final Function(int)? onTap;
+
+  // 메뉴 패딩
+  final EdgeInsetsGeometry? padding;
+
+  // 네비게이션 메뉴 ScrollPhysics
+  final ScrollPhysics? physics;
+
+  @override
+  State<SFNavigationMenu> createState() => _SFNavigationMenu();
+}
+
+class _SFNavigationMenu extends State<SFNavigationMenu> {
+  int focusedChild = 0;
+  List<bool> ishover = [];
+  @override
+  void initState() {
+    super.initState();
+    ishover = List.generate(widget.menu.length, (index) => false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: widget.width,
+      height: widget.height,
+      child: ListView.separated(
+        physics: widget.physics ?? const NeverScrollableScrollPhysics(),
+        scrollDirection: widget.direction,
+        itemCount: widget.menu.length,
+        itemBuilder: (context, index) => InkWell(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          onTap: () {
+            if (widget.onTap != null) {
+              widget.onTap!(index);
+            }
+            focusedChild = index;
+            setState(() {});
+          },
+          hoverColor: widget.focusedBackgroundColor ?? SFColor.primary5,
+          onHover: (value) {
+            ishover[index] = value;
+            setState(() {});
+          },
+          child: Container(
+            width: widget.menuWidth,
+            height: widget.menuHeigh,
+            padding: widget.padding ?? const EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+                color: focusedChild == index
+                    ? widget.focusedBackgroundColor ?? SFColor.primary5
+                    : widget.backgroundColor,
+                border: Border.all(
+                    width: widget.outlineWidth ?? 0,
+                    color: widget.outlineColor ?? Colors.transparent),
+                borderRadius: BorderRadius.circular(widget.radius)),
+            child: Text(
+              widget.menu[index],
+              style: SFTextStyle.b3M16(
+                  color: ishover[index]
+                      ? widget.focusedTextColor ?? SFColor.primary80
+                      : focusedChild == index
+                          ? widget.focusedTextColor ?? SFColor.primary80
+                          : widget.textColor ?? Colors.black),
+            ),
+          ),
+        ),
+        separatorBuilder: (context, index) =>
+            SizedBox(width: widget.menuSpacing),
+      ),
+    );
+  }
+}
