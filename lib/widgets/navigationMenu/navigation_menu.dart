c7975910df734362002983ed01977657786412f6
judy <kje010207@gmail.com>
2023-08-01 11:12:54 +0900

    judy, Feat:: navigationMenu에 메뉴 크기랑 간격에 따른 height 기본값 추가 #62

--- lib/widgets/navigationMenu/navigation_menu.dart
+++ lib/widgets/navigationMenu/navigation_menu.dart
@@ -6,7 +6,7 @@ class SFNavigationMenu extends StatefulWidget {
     super.key,
     required this.menu,
     this.width,
-    required this.height,
+    this.height,
     this.menuSize,
     this.menuSpacing = 10,
     this.backgroundColor,
@@ -21,6 +21,7 @@ class SFNavigationMenu extends StatefulWidget {
     this.padding,
     this.physics,
     this.initialIndex,
+    this.textStyle,
   });
   // 리스트 메뉴
   final List<String> menu;
@@ -29,13 +30,13 @@ class SFNavigationMenu extends StatefulWidget {
   final double? width;
 
   // 높이
-  final double height;
+  final double? height;
 
   // 메뉴 가로 크기
   final double? menuSize;
 
   // 메뉴 사이 spacing
-  final double? menuSpacing;
+  final double menuSpacing;
 
   // 메뉴 배경 색
   final Color? backgroundColor;
@@ -73,6 +74,9 @@ class SFNavigationMenu extends StatefulWidget {
   // 사적 인덱스
   final int? initialIndex;
 
+  // 메뉴 텍스트 스타일
+  final TextStyle? textStyle;
+
   @override
   State<SFNavigationMenu> createState() => _SFNavigationMenu();
 }
@@ -82,6 +86,8 @@ class _SFNavigationMenu extends State<SFNavigationMenu> {
   List<bool> ishover = [];
   double? widthSpacing;
   double? heightSpacing;
+  double? height;
+
   @override
   void initState() {
     super.initState();
@@ -94,69 +100,80 @@ class _SFNavigationMenu extends State<SFNavigationMenu> {
   Widget build(BuildContext context) {
     return SizedBox(
       width: widget.width,
-      height: widget.height,
-      child: ListView.separated(
-        physics: widget.physics ?? const NeverScrollableScrollPhysics(),
-        scrollDirection: widget.direction,
-        itemCount: widget.menu.length,
-        itemBuilder: (context, index) => InkWell(
-          splashColor: Colors.transparent, // 클릭할 때 나오는 효과 색상
-          highlightColor: Colors.transparent, // 클릭 유지 시 나오는 효과 색상
-          onTap: () {
-            if (widget.onTap != null) {
-              widget.onTap!(index);
-            }
-            focusedChild = index;
-            setState(() {});
-          },
-          hoverColor: widget.focusedBackgroundColor ?? SFColor.primary5,
-          onHover: (value) {
-            ishover[index] = value;
-            setState(() {});
-          },
-          borderRadius: BorderRadius.all(
-            Radius.circular(widget.radius),
-          ),
-          child: Ink(
-            width: widget.menuSize,
-            height: widget.menuSize,
-            padding: widget.padding ?? const EdgeInsets.all(12.0),
-            decoration: BoxDecoration(
-                color: focusedChild == index
-                    ? widget.focusedBackgroundColor ?? SFColor.primary5
-                    : widget.backgroundColor,
-                border: Border.all(
-                    width: widget.outlineWidth ?? 0,
-                    color: widget.outlineColor ?? Colors.transparent),
-                borderRadius: BorderRadius.circular(widget.radius)),
-            child: Center(
-              child: Text(
-                widget.menu[index],
-                style: SFTextStyle.b3M16(
-                    color: ishover[index]
-                        ? widget.focusedTextColor ?? SFColor.primary80
-                        : focusedChild == index
-                            ? widget.focusedTextColor ?? SFColor.primary80
-                            : widget.textColor ?? Colors.black),
+      height: widget.height ?? height,
+      child: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+        child: ListView.separated(
+          padding: EdgeInsets.zero,
+          physics: widget.physics ?? const NeverScrollableScrollPhysics(),
+          scrollDirection: widget.direction,
+          itemCount: widget.menu.length,
+          itemBuilder: (context, index) => InkWell(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onTap: () {
+              if (widget.onTap != null) {
+                widget.onTap!(index);
+              }
+              focusedChild = index;
+              setState(() {});
+            },
+            hoverColor: widget.focusedBackgroundColor ?? SFColor.primary5,
+            onHover: (value) {
+              ishover[index] = value;
+              setState(() {});
+            },
+            borderRadius: BorderRadius.all(
+              Radius.circular(widget.radius),
+            ),
+            child: Ink(
+              width: widget.menuSize,
+              height: widget.menuSize,
+              padding: widget.padding ?? const EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                  color: focusedChild == index
+                      ? widget.focusedBackgroundColor ?? SFColor.primary5
+                      : widget.backgroundColor,
+                  border: Border.all(
+                      width: widget.outlineWidth ?? 0,
+                      color: widget.outlineColor ?? Colors.transparent),
+                  borderRadius: BorderRadius.circular(widget.radius)),
+              child: Center(
+                child: Text(
+                  widget.menu[index],
+                  style: widget.textStyle ??
+                      SFTextStyle.b3M16(
+                          color: ishover[index]
+                              ? widget.focusedTextColor ?? SFColor.primary80
+                              : focusedChild == index
+                                  ? widget.focusedTextColor ?? SFColor.primary80
+                                  : widget.textColor ?? Colors.black),
+                ),
               ),
             ),
           ),
-        ),
-        separatorBuilder: (context, index) => SizedBox(
-          width: widthSpacing,
-          height: heightSpacing,
+          separatorBuilder: (context, index) => SizedBox(
+            width: widthSpacing,
+            height: heightSpacing,
+          ),
         ),
       ),
     );
   }
 
   spacing() {
+    double fontSize =
+        (widget.textStyle != null ? widget.textStyle!.fontSize ?? 16 : 16) +
+            4.8;
     switch (widget.direction) {
       case Axis.vertical:
         heightSpacing = widget.menuSpacing;
+        height = (heightSpacing! + (widget.menuSize ?? fontSize + 26)) *
+            widget.menu.length;
         break;
       case Axis.horizontal:
         widthSpacing = widget.menuSpacing;
+        height = fontSize + 26;
         break;
     }
   }
