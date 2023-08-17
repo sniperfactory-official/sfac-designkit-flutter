274c653a7d3c29279bde0c1fdb06d0f3a3d4fca6
judy <kje010207@gmail.com>
2023-08-17 14:32:12 +0900

    judy, Feat:: value 값 증가에 따라 그 전 value에서 증가하는 애니메이션 기능 추가 #87

--- lib/widgets/progressbar/progressbar.dart
+++ lib/widgets/progressbar/progressbar.dart
@@ -12,33 +12,42 @@ class SFProgressBar extends StatefulWidget {
     this.valueTextStyle,
     this.backgroundColor,
     this.valueBackgroundColor,
+    this.animation = true,
+    this.animationDuration,
     this.borderRadius = 16,
     this.textPadding = 16,
   })  : assert(value == null ? true : duration == null),
+        assert(value != null && value >= 0.0 && value <= 1),
         super(key: key);
 
-  // ProgressBar 높이
+  // 프로그래머스바 높이
   final double? height;
 
-  // ProgressBar
+  // 프로그래머스 값
   final double? value;
 
-  // ProgressBar 시간 증가에 따른 값 증가
+  // 프로그래머스 duration만큼 시간 증가에 따른 값 증가
   final Duration? duration;
 
-  // ProgressBar 텍스트 스타일
+  // 프로그래머스 텍스트 스타일
   final TextStyle? valueTextStyle;
 
-  // ProgressBar 배경색
+  // 프로그래머스 배경색
   final Color? backgroundColor;
 
-  // ProgressBar 값 배경색
+  // 프로그래머스 값 배경색
   final Color? valueBackgroundColor;
 
-  // ProgressBar 테두리 곡선
+  // value일 때 애니메이션 여부
+  final bool animation;
+
+  // value일 때 애니메이션 duraition
+  final Duration? animationDuration;
+
+  // 프로그래머스 테두리 곡선
   final double borderRadius;
 
-  // ProgressBar 텍스트 패딩
+  // 프로그래머스 텍스트 패딩
   final double textPadding;
 
   @override
@@ -49,22 +58,45 @@ class _SFProgressBarState extends State<SFProgressBar>
     with TickerProviderStateMixin {
   late TextPainter textPainter;
   late AnimationController _animationController;
+  late Animation _animation;
+  late double _progressPercent;
+
   @override
   void initState() {
     super.initState();
+    _progressPercent = widget.value ?? 0;
     _animationController = AnimationController(
       vsync: this,
-      duration: widget.duration ?? const Duration(seconds: 10),
+      duration: widget.duration ?? const Duration(seconds: 3),
     );
-
     _animationController.addListener(() {
       setState(() {
         // Call setState to trigger a rebuild when animation updates
       });
     });
-
+    if (widget.animation) {
+      _animation = Tween(begin: _progressPercent, end: widget.value)
+          .animate(_animationController)
+        ..addListener(() {
+          setState(() {
+            _progressPercent = _animation.value;
+          });
+        });
+    }
     if (widget.duration != null) {
       _animationController.forward();
+    }
+  }
+
+  @override
+  void didUpdateWidget(SFProgressBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.value != widget.value && widget.animation) {
+      _animationController.duration =
+          widget.animationDuration ?? const Duration(milliseconds: 300);
+      _animation = Tween(begin: oldWidget.value, end: widget.value)
+          .animate(_animationController);
+      _animationController.forward(from: 0);
     }
   }
 
@@ -78,7 +110,7 @@ class _SFProgressBarState extends State<SFProgressBar>
   Widget build(BuildContext context) {
     TextSpan textSpan = TextSpan(
       text: widget.value != null
-          ? '${(widget.value! * 100).round()}%'
+          ? '${((widget.animation ? _progressPercent : widget.value!) * 100).round()}%'
           : '${(_animationController.value * 100).toStringAsFixed(0)}%',
       style: widget.valueTextStyle ?? SFTextStyle.b3M16(color: Colors.white),
     );
@@ -94,7 +126,8 @@ class _SFProgressBarState extends State<SFProgressBar>
             builder: (BuildContext context, BoxConstraints constraints) {
               double width = constraints.maxWidth;
               double valueWidth = widget.value != null
-                  ? width * widget.value!
+                  ? width *
+                      (widget.animation ? _progressPercent : widget.value!)
                   : _animationController.value * width;
               return Container(
                 width: width,
