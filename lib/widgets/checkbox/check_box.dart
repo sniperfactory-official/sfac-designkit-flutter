b287f655558722c5bb40d1a7fa685e61eb699c55
2Juzzang <skyy630@naver.com>
2023-08-24 15:58:58 +0900

    juno, Feat:: add CheckBox widget #108

---
+++ lib/widgets/checkbox/check_box.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import '../../util/sfac_color.dart';
+
+enum SFCheckBoxType {
+  basic,
+  primary,
+}
+
+class SFCheckBox<T> extends StatefulWidget {
+  const SFCheckBox({
+    Key? key,
+    required this.onChanged,
+    this.label,
+    this.isChecked = false,
+    this.type = SFCheckBoxType.basic,
+    this.size = 16,
+    this.margin = const EdgeInsets.all(8),
+    this.radius = 4,
+    this.labelSpcing = 8,
+    this.labelStyle = const TextStyle(
+      color: SFColor.grayScale80,
+    ),
+  }) : super(key: key);
+
+  // CheckBox를 클릭했을 때 호출되는 이벤트 핸들러
+  // value는 bool 타입을 가짐
+  final ValueChanged<bool> onChanged;
+
+  // CheckBox의 부가 설명을 위한 값
+  final String? label;
+
+  // CheckBox의 타입으로 basic, primary가 있음
+  final SFCheckBoxType type;
+
+  // CheckBox의 크기
+  final double size;
+
+  // CheckBox 바깥의 여백
+  final EdgeInsets margin;
+
+  // CheckBox와 label 사이의 여백
+  final double labelSpcing;
+
+  // CheckBox 우측 value 텍스트의 스타일
+  final TextStyle? labelStyle;
+
+  // CheckBox border
+  final double radius;
+
+  final bool isChecked;
+
+  @override
+  State<SFCheckBox> createState() => _SFCheckBoxState();
+}
+
+class _SFCheckBoxState extends State<SFCheckBox> {
+  Color _backgroundColor = Colors.transparent;
+  Color _outlineColor = SFColor.grayScale100;
+  Color _iconColor = SFColor.primary60;
+  late bool isChecked;
+
+  @override
+  void initState() {
+    isChecked = widget.isChecked;
+    super.initState();
+  }
+
+  void onSwitch() {
+    setState(() {
+      isChecked = !isChecked;
+    });
+    widget.onChanged(isChecked);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    switch (widget.type) {
+      case SFCheckBoxType.basic:
+        break;
+      case SFCheckBoxType.primary:
+        _backgroundColor = SFColor.primary60;
+        _outlineColor = SFColor.primary60;
+        _iconColor = Colors.white;
+        break;
+    }
+    return Container(
+      margin: widget.margin,
+      child: GestureDetector(
+        onTap: onSwitch,
+        child: Row(
+          children: [
+            Container(
+              width: widget.size,
+              height: widget.size,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                border: Border.all(
+                    color: _outlineColor,
+                    strokeAlign: BorderSide.strokeAlignOutside),
+                borderRadius: BorderRadius.circular(widget.radius),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color:
+                      isChecked == true ? _backgroundColor : Colors.transparent,
+                  borderRadius: BorderRadius.circular(widget.radius),
+                ),
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    isChecked ? Icons.check_rounded : null,
+                    size: widget.size - 2,
+                    color: _iconColor,
+                  ),
+                ),
+              ),
+            ),
+            widget.label != null
+                ? Row(
+                    children: [
+                      SizedBox(
+                        width: widget.labelSpcing,
+                      ),
+                      Text(
+                        widget.label!,
+                        style: widget.labelStyle,
+                      ),
+                    ],
+                  )
+                : const SizedBox.shrink()
+          ],
+        ),
+      ),
+    );
+  }
+}
