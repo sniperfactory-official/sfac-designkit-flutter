4eb406c029fa6c708488dbd950ba8b9356120d4a
2Juzzang <skyy630@naver.com>
2023-08-25 14:46:37 +0900

    juno, Feat:: loading spinner #115

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sfac_design_flutter/sfac_design_flutter.dart';
+import 'package:sfac_design_flutter/widgets/loadingspinner/loading_spinner.dart';
 import 'package:sfac_design_flutter/widgets/searchfield/searchfield.dart';
 
 void main() {
@@ -19,117 +20,15 @@ class MyApp extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Row(
-                  children: [
-                    SFAvatar(
-                      backgroundColor: Colors.white,
-                      child: SvgPicture.asset('images/logo.svg'),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      '스나이퍼팩토리 디자인시스템',
-                      style: SFTextStyle.b2B18(),
-                    )
-                  ],
-                ),
-                const SFCard(
-                  margin: EdgeInsets.symmetric(vertical: 4),
-                  title: Text('Badge'),
-                  verticalSpacing: 8,
-                  content: Row(
-                    children: [
-                      SFBadge(
-                        child: Text(
-                          'primary',
-                        ),
-                      ),
-                      SizedBox(
-                        width: 4,
-                      ),
-                      SFBadge(
-                        status: SFBadgeStatus.secondary,
-                        child: Text(
-                          'secondary',
-                        ),
-                      ),
-                      SizedBox(
-                        width: 4,
-                      ),
-                      SFBadge(
-                        status: SFBadgeStatus.outline,
-                        child: Text(
-                          'outline',
-                        ),
-                      ),
-                      SizedBox(
-                        width: 4,
-                      ),
-                      SFBadge(
-                        status: SFBadgeStatus.destructive,
-                        child: Text(
-                          'destructive',
-                        ),
-                      ),
-                    ],
+                Center(
+                  child: const SFLoadingSpinner(
+                    color: Colors.amber,
+                    duration: Duration(seconds: 4),
+                    size: 120,
+                    strokeWidth: 24,
                   ),
-                ),
-                SFCard(
-                  margin: const EdgeInsets.symmetric(vertical: 4),
-                  verticalSpacing: 8,
-                  title: const Text('Button'),
-                  content: Column(
-                    children: [
-                      SFButton(
-                        onPressed: () {},
-                        child: const Text('primary'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.secondary,
-                        onPressed: () {},
-                        child: const Text('secondary'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.outline,
-                        onPressed: () {},
-                        child: const Text('outline'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.destructive,
-                        onPressed: () {},
-                        child: const Text('destructive'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.link,
-                        onPressed: () {},
-                        child: const Text('link'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.disabled,
-                        onPressed: () {},
-                        child: const Text('disabled'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.asChild,
-                        onPressed: () {},
-                        child: const Text('asChild'),
-                      ),
-                    ],
-                  ),
-                ),
-                const SFCard(
-                  margin: EdgeInsets.symmetric(vertical: 4),
-                  title: Text('Search Field'),
-                  content: SFSearchField(),
                 )
               ],
             ),
