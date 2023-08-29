5b113765701351d1084d7d3d704db2eec20a95b2
marco <tlstmdgh8620@gmail.com>
2023-08-29 13:33:18 +0900

    marco, Comment:: 슬라이드 위젯 주석변경 #105

--- lib/widgets/slider/slider.dart
+++ lib/widgets/slider/slider.dart
@@ -20,19 +20,19 @@ class SFSlider extends StatefulWidget {
   // 슬라이더 값이 변경될 때 호출되는 콜백
   final Function(int value) onChanged;
 
-  //너비
+  // 슬라이드 너비
   final double? width;
 
-  //원 사이즈
+  // 원 사이즈
   final double thumbSize;
 
-  //원 테두리 두께
+  // 원 테두리 두께
   final double thumbWidth;
 
-  //퍼센트
+  // 슬라이드 수치
   final int percentValue;
 
-  //percent 높이
+  // 슬라이드 높이
   final double? height;
 
   @override
@@ -61,83 +61,86 @@ class SFSliderState extends State<SFSlider> {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      builder:(context, constraints) {
+      builder: (context, constraints) {
         final width = widget.width ?? constraints.maxWidth;
         return SizedBox(
-        width: width,
-        height: widget.thumbSize,
-        child: GestureDetector(
-          onHorizontalDragUpdate: (details) {
-            RenderBox box = context.findRenderObject() as RenderBox;
-            double localDx = box.globalToLocal(details.globalPosition).dx;
-            double newValue =
-                (localDx / (box.size.width - widget.thumbSize)).clamp(0.0, 1.0);
-            _onDragUpdate(newValue);
-          },
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  height: widget.height,
-                  margin: EdgeInsets.symmetric(horizontal: widget.thumbSize / 2),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(999),
-                    color: SFColor.grayScale20,
-                  ),
-                ),
-              ),
-              Positioned(
-                left: widget.thumbSize / 2,
-                top: 0,
-                bottom: 0,
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    width: sliderValue*(width-widget.thumbSize),
-                    height: widget.height ?? 11,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(999),
-                          bottomLeft: Radius.circular(999)),
-                      color: SFColor.primary80,
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                left:sliderValue*(width-widget.thumbSize),
-                top: 0,
-                bottom: 0,
-                child: Align(
+          width: width,
+          height: widget.thumbSize,
+          child: GestureDetector(
+            onHorizontalDragUpdate: (details) {
+              RenderBox box = context.findRenderObject() as RenderBox;
+              double localDx = box.globalToLocal(details.globalPosition).dx;
+              double newValue = (localDx / (box.size.width - widget.thumbSize))
+                  .clamp(0.0, 1.0);
+              _onDragUpdate(newValue);
+            },
+            child: Stack(
+              children: [
+                Align(
                   alignment: Alignment.center,
                   child: Container(
-                    width: widget.thumbSize,
-                    height: widget.thumbSize,
+                    height: widget.height,
+                    margin:
+                        EdgeInsets.symmetric(horizontal: widget.thumbSize / 2),
                     decoration: BoxDecoration(
-                      border: Border.all(
-                        color: SFColor.primary80,
-                        width: widget.thumbWidth,
-                      ),
                       borderRadius: BorderRadius.circular(999),
+                      color: SFColor.grayScale20,
                     ),
-                    child: Center(
-                      child: Container(
-                        width: widget.thumbSize - 1,
-                        height: widget.thumbSize - 1,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(999),
+                  ),
+                ),
+                Positioned(
+                  left: widget.thumbSize / 2,
+                  top: 0,
+                  bottom: 0,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      width: sliderValue * (width - widget.thumbSize),
+                      height: widget.height,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(999),
+                          bottomLeft: Radius.circular(999),
+                        ),
+                        color: SFColor.primary80,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: sliderValue * (width - widget.thumbSize),
+                  top: 0,
+                  bottom: 0,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: widget.thumbSize,
+                      height: widget.thumbSize,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: SFColor.primary80,
+                          width: widget.thumbWidth,
+                        ),
+                        borderRadius: BorderRadius.circular(999),
+                      ),
+                      child: Center(
+                        child: Container(
+                          width: widget.thumbSize - 1,
+                          height: widget.thumbSize - 1,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(999),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
-        ),
-      );},
+        );
+      },
     );
   }
 }
