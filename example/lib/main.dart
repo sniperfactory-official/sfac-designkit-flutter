478359076424433cc3a4beb348d5caa7926443b2
2Juzzang <skyy630@naver.com>
2023-10-18 11:49:50 +0900

    juno, Feat:: login 기능 추가 #3

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,121 +20,24 @@ class MyApp extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    const SFAvatar(
-                      backgroundColor: Colors.white,
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      '스나이퍼팩토리 디자인시스템',
-                      style: SFTextStyle.b2B18(),
-                    )
-                  ],
-                ),
-                const SFCard(
-                  margin: EdgeInsets.symmetric(vertical: 4),
-                  title: Text('Badge'),
-                  verticalSpacing: 8,
-                  content: Row(
-                    children: [
-                      SFBadge(
-                        child: Text(
-                          'primary',
-                        ),
-                      ),
-                      SizedBox(
-                        width: 4,
-                      ),
-                      SFBadge(
-                        status: SFBadgeStatus.secondary,
-                        child: Text(
-                          'secondary',
-                        ),
-                      ),
-                      SizedBox(
-                        width: 4,
-                      ),
-                      SFBadge(
-                        status: SFBadgeStatus.outline,
-                        child: Text(
-                          'outline',
-                        ),
-                      ),
-                      SizedBox(
-                        width: 4,
-                      ),
-                      SFBadge(
-                        status: SFBadgeStatus.destructive,
-                        child: Text(
-                          'destructive',
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SFCard(
-                  margin: const EdgeInsets.symmetric(vertical: 4),
-                  verticalSpacing: 8,
-                  title: const Text('Button'),
-                  content: Column(
-                    children: [
-                      SFButton(
-                        onPressed: () {},
-                        child: const Text('primary'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.secondary,
-                        onPressed: () {},
-                        child: const Text('secondary'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.outline,
-                        onPressed: () {},
-                        child: const Text('outline'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.destructive,
-                        onPressed: () {},
-                        child: const Text('destructive'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.link,
-                        onPressed: () {},
-                        child: const Text('link'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.disabled,
-                        onPressed: () {},
-                        child: const Text('disabled'),
-                      ),
-                      const SizedBox(height: 8),
-                      SFButton(
-                        status: SFButtonStatus.asChild,
-                        onPressed: () {},
-                        child: const Text('asChild'),
-                      ),
-                    ],
-                  ),
-                ),
-                const SFCard(
-                  margin: EdgeInsets.symmetric(vertical: 4),
-                  title: Text('Search Field'),
-                  content: SFSearchField(),
-                )
-              ],
+              children: [TTTT()],
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class TTTT extends StatelessWidget {
+  const TTTT({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SFDatePicker(
+        type: SFCalendarType.range,
+        getSelectedDate: (start, end, selectedDateList, selectedOne) {
+          alertDialog(context, title: 'title', content: 'content');
+        });
   }
 }
